d8ae0712f4ffd8985b6ca41deb5d8a3e9577c453
jsipac-2022133 <jsipac-2022133@kinal.edu.gt>
2026-04-07 20:01:45 -0600

    feat: Implementación de Association para representar pares frances-español

---
+++ src/Association.java
@@ -0,0 +1,27 @@
+public class Association<K extends Comparable<K>, V> {
+
+    private K key;    // frances
+    private V value;  // español
+
+    public Association(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+}
